4ecdafe5ee7eb7d074f98a682ee6a2574cdd285f
Mike Tsvirkunov <mike.tsvirkunov@gmail.com>
2023-06-30 23:56:56 +0600

    complete_main_info

--- app/src/main/java/com/example/practica_2023/MainActivity.kt
+++ app/src/main/java/com/example/practica_2023/MainActivity.kt
@@ -3,14 +3,8 @@ package com.example.practica_2023
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.example.practica_2023.fragments.MainFragment
 import com.example.practica_2023.databinding.ActivityMainBinding
-import com.google.android.gms.location.LocationServices
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,24 +14,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportFragmentManager.beginTransaction().replace(R.id.PlaceHolder, MainFragment.newInstance()).commit()
-//        getWeatherReport("London")
     }
-//    private fun getWeatherReport(place: String){
-//        val url = "https://api.openweathermap.org/data/2.5/" +
-//                "weather?lat=44.34&lon=10.99&" +
-//                "appid=${getString(R.string.api_key)}"
-//        val queue = Volley.newRequestQueue(this)
-//        val weatherReportGetRequest = StringRequest(
-//            Request.Method.GET,
-//            url,
-//            {
-//                    response->
-//                val weatherReport = JSONObject(response)
-//                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-//                Log.d("RequestLog", "Weather request get success: $fusedLocationClient")
-//                Log.d("RequestLog", "Weather request get success: $response")
-//            },
-//            {Log.d("RequestLog", "Weather request get error: $it")})
-//        queue.add(weatherReportGetRequest)
-//    }
 }
--- app/src/main/java/com/example/practica_2023/fragments/MainFragment.kt
+++ app/src/main/java/com/example/practica_2023/fragments/MainFragment.kt
@@ -8,18 +8,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.practica_2023.R
-import com.example.practica_2023.databinding.ActivityMainBinding
+//import com.example.practica_2023.databinding.ActivityMainBinding
 //import com.example.practica_2023.Manifest
 import com.example.practica_2023.databinding.FragmentMainBinding
 import com.google.android.gms.location.LocationServices
 import org.json.JSONObject
+import java.util.Dictionary
 
 
 class MainFragment : Fragment() {
@@ -30,15 +33,24 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
+
+//        getWeatherReport(getCurrentApiLink("London"))
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         permissionCheck()
-        getWeatherReport("London")
-        binding.GetWeatherReportBtn.setOnClickListener{
-            getWeatherReport("London")
+
+        val x = resources.getStringArray(R.array.places)
+        val arrayAdapter = ArrayAdapter(requireContext(),
+            R.layout.city_check_layout,
+            x)
+        binding.PlaceChecker.adapter = arrayAdapter
+        getWeatherReport(getCurrentApiLink(binding.PlaceChecker.selectedItem.toString()))
+        binding.swiperefresh.setOnRefreshListener{
+            getWeatherReport(getCurrentApiLink(binding.PlaceChecker.selectedItem.toString()))
+            binding.swiperefresh.isRefreshing = false
         }
     }
 
@@ -54,10 +66,13 @@ class MainFragment : Fragment() {
             pLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
-    private fun getWeatherReport(place: String){
-        val url = "https://api.openweathermap.org/data/2.5/" +
-                "weather?lat=44.34&lon=10.99&" +
-                "appid=${getString(R.string.api_key)}"
+    private fun getCurrentApiLink(place: String): String{
+        return "https://api.openweathermap.org/data/2.5/" +
+                "weather?q=${place}&" +
+                "&units=${getString(R.string.api_units)}" +
+                "&appid=${getString(R.string.api_key)}"
+    }
+    private fun getWeatherReport(url: String){
         val queue = Volley.newRequestQueue(context)
         val weatherReportGetRequest = StringRequest(
             Request.Method.GET,
@@ -65,16 +80,23 @@ class MainFragment : Fragment() {
             {
                     response->
                 val weatherReport = JSONObject(response)
-                UpdateMainInfo(weatherReport)
+                updateMainInfo(weatherReport)
                 Log.d("RequestLog", "Weather request get success: $response")
             },
             {Log.d("RequestLog", "Weather request get error: $it")})
         queue.add(weatherReportGetRequest)
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun UpdateMainInfo(weatherInfo: JSONObject){
+    @SuppressLint("SetTextI18n", "DiscouragedApi")
+    private fun updateMainInfo(weatherInfo: JSONObject){
         binding.WindSpeedNumLabel.text = "${weatherInfo.getJSONObject("wind").getString("speed")} m/s"
+        binding.WeatherConditionLabel.text = JSONObject(weatherInfo.getJSONArray("weather")[0].toString()).getString("main")
+        binding.TempInfoLabel.text = "${weatherInfo.getJSONObject("main").getString("temp")} ºC"
+        binding.HumidityInfo.text = "${weatherInfo.getJSONObject("main").getString("humidity")} %"
+        Log.d("JSONLog", "Weather : ${weatherInfo.getJSONArray("weather")[0]}")
+        binding.WeatherImg.setImageResource(resources.getIdentifier(JSONObject(weatherInfo.getJSONArray("weather")[0].toString()).getString("main").lowercase(), "drawable",
+            context?.packageName ?: ""
+        ))
     }
 
     companion object {
